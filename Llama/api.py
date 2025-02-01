19c3d604a0fd99712c28fadb41938776d40829e3
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-02-01 20:00:19 +0900

    Database 연결 추가

--- Llama/api.py
+++ Llama/api.py
@@ -1,24 +1,30 @@
-from fastapi import FastAPI, HTTPException, Query
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.responses import PlainTextResponse, JSONResponse
 from Llama.LlamaResponseModel import LlamaResponse, Message
 import requests
-import markdown2
+import os
+
+from dotenv import load_dotenv
+from database.connect.deps import get_db
+from database.models.markdown_result import MarkeDownResult, create_mark_down_result
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+load_dotenv()
+
 # Llama 컨테이너의 URL 및 포트 설정
-LLAMA_API_URL = "http://localhost:11434/api/chat"
+LLAMA_API_URL = f"http://{os.getenv('LLAMA_HOST')}:{os.getenv('LLAMA_PORT')}/api/chat"
 
 @app.get("/", response_class=JSONResponse) 
 def read_root():
     return {"message": "Hello, World!"}
 
-@app.post("/ask", response_class=HTMLResponse)
-async def ask_llama(question: Message, lang: str = Query("en")):
+@app.post("/ask", response_class=PlainTextResponse)
+async def ask_llama(question: Message, lang: str = Query("en"), db: Session = Depends(get_db)):
     """
     Llama 컨테이너에 질문을 보내고 Markdown 형식으로 응답을 반환합니다.
     """
-    # Llama API에 요청 보내기
     try:
         payload = {
             "model": "llama3",
@@ -29,20 +35,19 @@ async def ask_llama(question: Message, lang: str = Query("en")):
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=500, detail=f"Llama API 호출 실패: {e}")
 
-    # NDJSON 응답 처리 (스트리밍 방식)
     try:
-        html_content_list = []
+        markdown_content_list = []
 
         for line in response.iter_lines(decode_unicode=True):
-            if line.strip():  # 빈 줄 무시
-                # 각 줄을 JSON으로 파싱하여 Pydantic 모델로 변환
+            if line.strip():
                 llama_response = LlamaResponse.parse_raw(line)
-                html_content_list.append(markdown2.markdown(llama_response.message.content))
+                markdown_content_list.append(llama_response.message.content)
 
-        # 여러 응답을 HTML로 결합 (구분선 추가)
-        html_content = "".join(html_content_list)
+        # 마크다운 응답들을 하나로 결합
+        markdown_content = "".join(markdown_content_list)
+        create_mark_down_result(db, MarkeDownResult(markdown=markdown_content))
 
-        return HTMLResponse(content=html_content)
+        return PlainTextResponse(content=markdown_content)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"NDJSON 처리 중 오류 발생: {e}")
